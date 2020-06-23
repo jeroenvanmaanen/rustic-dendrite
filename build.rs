2adc6dcf0ea39e167882f1ef53ac1dccb69524b7
Jeroen van Maanen <jeroen@leialearns.org>
2020-06-23 08:12:33 +0200

    Added "grpc_example.proto". Added implementation of GreeterService#greet to "server.rs".

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::compile_protos("proto/hello_world.proto")?;
+    tonic_build::compile_protos("proto/grpc_example.proto")?;
     Ok(())
 }
--- src/server.rs
+++ src/server.rs
@@ -3,8 +3,12 @@ use tonic::{transport::Server, Request, Response, Status};
 use hello_world::greeter_server::{Greeter, GreeterServer};
 use hello_world::{HelloReply, HelloRequest};
 
+use hello_world::greeter_service_server::{GreeterService, GreeterServiceServer};
+use hello_world::{Acknowledgement,Empty,Greeting};
+
 pub mod hello_world {
     tonic::include_proto!("hello_world"); // The string specified here must match the proto package name
+    tonic::include_proto!("grpc_example");
 }
 #[derive(Debug, Default)]
 pub struct MyGreeter {}
@@ -25,13 +29,31 @@ impl Greeter for MyGreeter {
     }
 }
 
+#[tonic::async_trait]
+impl GreeterService for MyGreeter {
+    async fn greet(
+        &self,
+        request: Request<Greeting>,
+    ) -> Result<Response<Acknowledgement>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let reply = hello_world::Acknowledgement {
+            message: format!("Hello {}!", request.into_inner().message).into(),
+        };
+
+        Ok(Response::new(reply))
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "0.0.0.0:50051".parse()?;
     let greeter = MyGreeter::default();
+    let greeter_service = MyGreeter::default();
 
     Server::builder()
         .add_service(GreeterServer::new(greeter))
+        .add_service(GreeterServiceServer::new(greeter_service))
         .serve(addr)
         .await?;
 
