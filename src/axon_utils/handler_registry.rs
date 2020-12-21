626b6df8c7331160cab79ae3012b683d4e290d6b
Jeroen van Maanen <jeroen@leialearns.org>
2020-12-21 22:40:41 +0100

    Moved HandlerRegistry to its own module.

---
+++ src/axon_utils/handler_registry.rs
@@ -0,0 +1,195 @@
+use anyhow::{anyhow,Result};
+use bytes::Bytes;
+use futures_core::Future;
+use futures_util::__private::Pin;
+use prost::DecodeError;
+use std::collections::HashMap;
+
+// I tried to make it possible to pass an `async fn` directly to parameter `handler`, but the return
+// type after desugaring is unnameable
+// (https://internals.rust-lang.org/t/naming-the-return-type-of-an-async-function/10085).
+// So it has to be wrapped in a closure that `Box::pin`s the return value, lik this:
+//
+// ```rust
+//     handler_registry.insert(
+//         "GreetCommand",
+//         &GreetCommand::decode,
+//         &(|c| Box::pin(handle_greet_command(c)))
+//     )?;
+// ```
+pub trait HandlerRegistry<W>: Send {
+    fn insert<T: Send + Clone>(
+        &mut self,
+        name: &str,
+        deserializer: &'static (dyn Fn(Bytes) -> Result<T,prost::DecodeError> + Sync),
+        handler: &'static (dyn Fn(T) -> Pin<Box<dyn Future<Output=Result<()>> + Send>> + Sync)
+    ) -> Result<()>;
+    fn insert_ignoring_output<T: Send + Clone, R: Clone>(
+        &mut self,
+        name: &str,
+        deserializer: &'static (dyn Fn(Bytes) -> Result<T,prost::DecodeError> + Sync),
+        handler: &'static (dyn Fn(T) -> Pin<Box<dyn Future<Output=Result<Option<R>>> + Send>> + Sync),
+    ) -> Result<()>;
+    fn insert_with_output<T: Send + Clone, R: Clone>(
+        &mut self,
+        name: &str,
+        deserializer: &'static (dyn Fn(Bytes) -> Result<T,prost::DecodeError> + Sync),
+        handler: &'static (dyn Fn(T) -> Pin<Box<dyn Future<Output=Result<Option<R>>> + Send>> + Sync),
+        type_name: &str,
+        wrapper: &'static (dyn Fn(&str, &R) -> Result<W> + Sync)
+    ) -> Result<()>;
+    fn get(&self, name: &str) -> Option<&Box<dyn SubscriptionHandle<W>>>;
+}
+
+pub struct TheHandlerRegistry<W: Clone> {
+    pub handlers: HashMap<String,Box<dyn SubscriptionHandle<W>>>,
+}
+
+impl<W: Clone + 'static> HandlerRegistry<W> for TheHandlerRegistry<W> {
+    fn insert<T: Send + Clone>(
+        &mut self,
+        name: &str,
+        deserializer: &'static (dyn Fn(Bytes) -> Result<T, DecodeError> + Sync),
+        handler: &'static (dyn Fn(T) -> Pin<Box<dyn Future<Output=Result<()>> + Send>> + Sync)
+    ) -> Result<()> {
+        let name = name.to_string();
+        let key = name.clone();
+        let handle: Box<dyn SubscriptionHandle<W>> = Box::new(SubscriptionVoid{
+            name,
+            deserializer,
+            handler,
+        });
+        if (*self).handlers.contains_key(&key) {
+            return Err(anyhow!("Handler already registered: {:?}", key))
+        }
+        (*self).handlers.insert(key.clone(), handle.box_clone());
+        Ok(())
+    }
+
+    fn insert_ignoring_output<T: Send + Clone, R: Clone>(
+        &mut self,
+        name: &str,
+        deserializer: &'static (dyn Fn(Bytes) -> Result<T, DecodeError> + Sync),
+        handler: &'static (dyn Fn(T) -> Pin<Box<dyn Future<Output=Result<Option<R>>> + Send>> + Sync),
+    ) -> Result<()> {
+        let name = name.to_string();
+        let key = name.clone();
+        let handle: Box<dyn SubscriptionHandle<W>> = Box::new(Subscription{
+            name,
+            deserializer,
+            handler,
+            wrapper: None,
+        });
+        if (*self).handlers.contains_key(&key) {
+            return Err(anyhow!("Handler already registered: {:?}", key))
+        }
+        (*self).handlers.insert(key.clone(), handle.box_clone());
+        Ok(())
+    }
+
+    fn insert_with_output<T: Send + Clone, R: Clone>(
+        &mut self,
+        name: &str,
+        deserializer: &'static (dyn Fn(Bytes) -> Result<T, DecodeError> + Sync),
+        handler: &'static (dyn Fn(T) -> Pin<Box<dyn Future<Output=Result<Option<R>>> + Send>> + Sync),
+        type_name: &str,
+        wrapper: &'static (dyn Fn(&str,&R) -> Result<W> + Sync)
+    ) -> Result<()> {
+        let name = name.to_string();
+        let key = name.clone();
+        let handle: Box<dyn SubscriptionHandle<W>> = Box::new(Subscription{
+            name,
+            deserializer,
+            handler,
+            wrapper: Some(ResponseWrapper {
+                type_name: type_name.to_string(),
+                convert: wrapper
+            }),
+        });
+        if (*self).handlers.contains_key(&key) {
+            return Err(anyhow!("Handler already registered: {:?}", key))
+        }
+        (*self).handlers.insert(key.clone(), handle.box_clone());
+        Ok(())
+    }
+
+    fn get(&self, name: &str) -> Option<&Box<dyn SubscriptionHandle<W>>> {
+        (*self).handlers.get(name)
+    }
+}
+
+pub fn empty_handler_registry<W: Clone>() -> TheHandlerRegistry<W> {
+    TheHandlerRegistry {
+        handlers: HashMap::new(),
+    }
+}
+
+#[tonic::async_trait]
+pub trait SubscriptionHandle<W>: Send + Sync {
+    fn name(&self) -> String;
+    async fn handle(&self, buf: Vec<u8>) -> Result<Option<W>>;
+    fn box_clone(&self) -> Box<dyn SubscriptionHandle<W>>;
+}
+
+#[derive(Clone)]
+struct Subscription<'a, T, R, W>
+{
+    pub name: String,
+    pub deserializer: &'a (dyn Fn(Bytes) -> Result<T,prost::DecodeError> + Sync),
+    pub handler: &'a (dyn Fn(T) -> Pin<Box<dyn Future<Output=Result<Option<R>>> + Send>> + Sync),
+    pub wrapper: Option<ResponseWrapper<'a, R, W>>,
+}
+
+#[derive(Clone)]
+struct ResponseWrapper<'a, R, W> {
+    pub type_name: String,
+    pub convert: &'a (dyn Fn(&str, &R) -> Result<W> + Sync),
+}
+
+#[tonic::async_trait]
+impl<T: Send + Clone, R: Clone, W: Clone> SubscriptionHandle<W> for Subscription<'static, T, R, W>
+{
+    fn name(&self) -> String {
+        self.name.clone()
+    }
+
+    async fn handle(&self, buf: Vec<u8>) -> Result<Option<W>> {
+        let message: T = (self.deserializer)(Bytes::from(buf))?;
+        if let Some(result) = (self.handler)(message).await? {
+            if let Some(wrapper) = self.wrapper.as_ref() {
+                return Ok(Some((wrapper.convert)(&wrapper.type_name, &result)?));
+            }
+        }
+        Ok(None)
+    }
+
+    fn box_clone(&self) -> Box<dyn SubscriptionHandle<W>> {
+        Box::from(Subscription::clone(&self))
+    }
+}
+
+#[derive(Clone)]
+struct SubscriptionVoid<'a, T>
+{
+    pub name: String,
+    pub deserializer: &'a (dyn Fn(Bytes) -> Result<T,prost::DecodeError> + Sync),
+    pub handler: &'a (dyn Fn(T) -> Pin<Box<dyn Future<Output=Result<()>> + Send>> + Sync),
+}
+
+#[tonic::async_trait]
+impl<T: Send + Clone, W: Clone + 'static> SubscriptionHandle<W> for SubscriptionVoid<'static, T>
+{
+    fn name(&self) -> String {
+        self.name.clone()
+    }
+
+    async fn handle(&self, buf: Vec<u8>) -> Result<Option<W>> {
+        let message: T = (self.deserializer)(Bytes::from(buf))?;
+        (self.handler)(message).await?;
+        Ok(None)
+    }
+
+    fn box_clone(&self) -> Box<dyn SubscriptionHandle<W>> {
+        Box::from(SubscriptionVoid::clone(&self))
+    }
+}
--- src/axon_utils/mod.rs
+++ src/axon_utils/mod.rs
@@ -1,10 +1,6 @@
 use anyhow::{anyhow,Result};
-use bytes::Bytes;
-use futures_core::Future;
-use futures_util::__private::Pin;
 use log::debug;
-use prost::{Message, DecodeError};
-use std::collections::HashMap;
+use prost::Message;
 use tonic::transport::Channel;
 
 use crate::axon_server::SerializedObject;
@@ -12,10 +8,13 @@ use crate::axon_server::SerializedObject;
 mod command_submit;
 mod command_worker;
 mod connection;
+mod handler_registry;
 
 pub use command_submit::init as init_command_sender;
 pub use command_worker::command_worker as command_worker;
 pub use connection::wait_for_server as wait_for_server;
+pub use handler_registry::empty_handler_registry as empty_handler_registry;
+pub use handler_registry::HandlerRegistry as HandlerRegistry;
 
 #[derive(Debug, Clone)]
 pub struct AxonServerHandle {
@@ -27,195 +26,6 @@ pub struct AxonServerHandle {
 pub struct AxonConnection {
     pub id: String,
     pub conn: Channel,
-}
-
-// I tried to make it possible to pass an `async fn` directly to parameter `handler`, but the return
-// type after desugaring is unnameable
-// (https://internals.rust-lang.org/t/naming-the-return-type-of-an-async-function/10085).
-// So it has to be wrapped in a closure that `Box::pin`s the return value, lik this:
-//
-// ```rust
-//     handler_registry.insert(
-//         "GreetCommand",
-//         &GreetCommand::decode,
-//         &(|c| Box::pin(handle_greet_command(c)))
-//     )?;
-// ```
-pub trait HandlerRegistry<W>: Send {
-    fn insert<T: Send + Clone>(
-        &mut self,
-        name: &str,
-        deserializer: &'static (dyn Fn(Bytes) -> Result<T,prost::DecodeError> + Sync),
-        handler: &'static (dyn Fn(T) -> Pin<Box<dyn Future<Output=Result<()>> + Send>> + Sync)
-    ) -> Result<()>;
-    fn insert_ignoring_output<T: Send + Clone, R: Clone>(
-        &mut self,
-        name: &str,
-        deserializer: &'static (dyn Fn(Bytes) -> Result<T,prost::DecodeError> + Sync),
-        handler: &'static (dyn Fn(T) -> Pin<Box<dyn Future<Output=Result<Option<R>>> + Send>> + Sync),
-    ) -> Result<()>;
-    fn insert_with_output<T: Send + Clone, R: Clone>(
-        &mut self,
-        name: &str,
-        deserializer: &'static (dyn Fn(Bytes) -> Result<T,prost::DecodeError> + Sync),
-        handler: &'static (dyn Fn(T) -> Pin<Box<dyn Future<Output=Result<Option<R>>> + Send>> + Sync),
-        type_name: &str,
-        wrapper: &'static (dyn Fn(&str, &R) -> Result<W> + Sync)
-    ) -> Result<()>;
-    fn get(&self, name: &str) -> Option<&Box<dyn SubscriptionHandle<W>>>;
-}
-
-pub struct TheHandlerRegistry<W: Clone> {
-    handlers: HashMap<String,Box<dyn SubscriptionHandle<W>>>,
-}
-
-impl<W: Clone + 'static> HandlerRegistry<W> for TheHandlerRegistry<W> {
-    fn insert<T: Send + Clone>(
-        &mut self,
-        name: &str,
-        deserializer: &'static (dyn Fn(Bytes) -> Result<T, DecodeError> + Sync),
-        handler: &'static (dyn Fn(T) -> Pin<Box<dyn Future<Output=Result<()>> + Send>> + Sync)
-    ) -> Result<()> {
-        let name = name.to_string();
-        let key = name.clone();
-        let handle: Box<dyn SubscriptionHandle<W>> = Box::new(SubscriptionVoid{
-            name,
-            deserializer,
-            handler,
-        });
-        if (*self).handlers.contains_key(&key) {
-            return Err(anyhow!("Handler already registered: {:?}", key))
-        }
-        (*self).handlers.insert(key.clone(), handle.box_clone());
-        Ok(())
-    }
-
-    fn insert_ignoring_output<T: Send + Clone, R: Clone>(
-        &mut self,
-        name: &str,
-        deserializer: &'static (dyn Fn(Bytes) -> Result<T, DecodeError> + Sync),
-        handler: &'static (dyn Fn(T) -> Pin<Box<dyn Future<Output=Result<Option<R>>> + Send>> + Sync),
-    ) -> Result<()> {
-        let name = name.to_string();
-        let key = name.clone();
-        let handle: Box<dyn SubscriptionHandle<W>> = Box::new(Subscription{
-            name,
-            deserializer,
-            handler,
-            wrapper: None,
-        });
-        if (*self).handlers.contains_key(&key) {
-            return Err(anyhow!("Handler already registered: {:?}", key))
-        }
-        (*self).handlers.insert(key.clone(), handle.box_clone());
-        Ok(())
-    }
-
-    fn insert_with_output<T: Send + Clone, R: Clone>(
-        &mut self,
-        name: &str,
-        deserializer: &'static (dyn Fn(Bytes) -> Result<T, DecodeError> + Sync),
-        handler: &'static (dyn Fn(T) -> Pin<Box<dyn Future<Output=Result<Option<R>>> + Send>> + Sync),
-        type_name: &str,
-        wrapper: &'static (dyn Fn(&str,&R) -> Result<W> + Sync)
-    ) -> Result<()> {
-        let name = name.to_string();
-        let key = name.clone();
-        let handle: Box<dyn SubscriptionHandle<W>> = Box::new(Subscription{
-            name,
-            deserializer,
-            handler,
-            wrapper: Some(ResponseWrapper {
-                type_name: type_name.to_string(),
-                convert: wrapper
-            }),
-        });
-        if (*self).handlers.contains_key(&key) {
-            return Err(anyhow!("Handler already registered: {:?}", key))
-        }
-        (*self).handlers.insert(key.clone(), handle.box_clone());
-        Ok(())
-    }
-
-    fn get(&self, name: &str) -> Option<&Box<dyn SubscriptionHandle<W>>> {
-        (*self).handlers.get(name)
-    }
-}
-
-pub fn empty_handler_registry<W: Clone>() -> TheHandlerRegistry<W> {
-    TheHandlerRegistry {
-        handlers: HashMap::new(),
-    }
-}
-
-#[tonic::async_trait]
-pub trait SubscriptionHandle<W>: Send + Sync {
-    fn name(&self) -> String;
-    async fn handle(&self, buf: Vec<u8>) -> Result<Option<W>>;
-    fn box_clone(&self) -> Box<dyn SubscriptionHandle<W>>;
-}
-
-#[derive(Clone)]
-struct Subscription<'a, T, R, W>
-{
-    pub name: String,
-    pub deserializer: &'a (dyn Fn(Bytes) -> Result<T,prost::DecodeError> + Sync),
-    pub handler: &'a (dyn Fn(T) -> Pin<Box<dyn Future<Output=Result<Option<R>>> + Send>> + Sync),
-    pub wrapper: Option<ResponseWrapper<'a, R, W>>,
-}
-
-#[derive(Clone)]
-struct ResponseWrapper<'a, R, W> {
-    pub type_name: String,
-    pub convert: &'a (dyn Fn(&str, &R) -> Result<W> + Sync),
-}
-
-#[tonic::async_trait]
-impl<T: Send + Clone, R: Clone, W: Clone> SubscriptionHandle<W> for Subscription<'static, T, R, W>
-{
-    fn name(&self) -> String {
-        self.name.clone()
-    }
-
-    async fn handle(&self, buf: Vec<u8>) -> Result<Option<W>> {
-        let message: T = (self.deserializer)(Bytes::from(buf))?;
-        if let Some(result) = (self.handler)(message).await? {
-            if let Some(wrapper) = self.wrapper.as_ref() {
-                return Ok(Some((wrapper.convert)(&wrapper.type_name, &result)?));
-            }
-        }
-        Ok(None)
-    }
-
-    fn box_clone(&self) -> Box<dyn SubscriptionHandle<W>> {
-        Box::from(Subscription::clone(&self))
-    }
-}
-
-#[derive(Clone)]
-struct SubscriptionVoid<'a, T>
-{
-    pub name: String,
-    pub deserializer: &'a (dyn Fn(Bytes) -> Result<T,prost::DecodeError> + Sync),
-    pub handler: &'a (dyn Fn(T) -> Pin<Box<dyn Future<Output=Result<()>> + Send>> + Sync),
-}
-
-#[tonic::async_trait]
-impl<T: Send + Clone, W: Clone + 'static> SubscriptionHandle<W> for SubscriptionVoid<'static, T>
-{
-    fn name(&self) -> String {
-        self.name.clone()
-    }
-
-    async fn handle(&self, buf: Vec<u8>) -> Result<Option<W>> {
-        let message: T = (self.deserializer)(Bytes::from(buf))?;
-        (self.handler)(message).await?;
-        Ok(None)
-    }
-
-    fn box_clone(&self) -> Box<dyn SubscriptionHandle<W>> {
-        Box::from(SubscriptionVoid::clone(&self))
-    }
 }
 
 pub trait VecU8Message {
@@ -233,13 +43,6 @@ where T: Message + Sized
 #[tonic::async_trait]
 pub trait CommandSink {
     async fn send_command(&self, command_type: &str, command: Box<&(dyn VecU8Message + Sync)>) -> Result<Option<SerializedObject>>;
-}
-
-pub fn proto_encode<T: Message>(message: T) -> Result<Box<Vec<u8>>> {
-    debug!("Encode output: {:?}", message);
-    let mut buf = Vec::new();
-    message.encode(&mut buf)?;
-    Ok(Box::from(buf))
 }
 
 pub fn axon_serialize<T: Message>(type_name: &str, message: &T) -> Result<SerializedObject> {
